86cdf68d394aeb4d08f6ec3f2230b9333184c67a
Edson Santos <129888422+Edsonsantos0840@users.noreply.github.com>
2025-03-19 20:19:47 -0300

    reajustando cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,40 +1,40 @@
-import { CorsOptions } from "cors";
-
-export const corsConfig: CorsOptions = {
-   origin: function(origin, callback){
-
-    const whiteList: string[] = []
-    whiteList.push(process.env.FRONTEND_URL)
-
-    if(process.argv[2] === '--api'){
-        whiteList.push(undefined)
-    }
-      
-    if(whiteList.includes(origin)){
-        callback(null, true)
-    }else {
-        callback(new Error('Erro de Cors'))
-    }
-   }
-}
-
 // import { CorsOptions } from "cors";
 
 // export const corsConfig: CorsOptions = {
-//   origin: function (origin, callback) {
-//     const whiteList: string[] = [process.env.FRONTEND_URL];
+//    origin: function(origin, callback){
 
-//     // Permitir undefined (caso de testes locais ou API interna)
-//     if (process.env.NODE_ENV === "development") {
-//       whiteList.push(undefined);
-//     }
+//     const whiteList: string[] = []
+//     whiteList.push(process.env.FRONTEND_URL)
 
-//     if (whiteList.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       console.error(`⚠️ Bloqueio CORS para origem: ${origin}`);
-//       callback(new Error("Erro de CORS: Origem não permitida"));
+//     if(process.argv[2] === '--api'){
+//         whiteList.push(undefined)
 //     }
-//   },
-//   credentials: true, // Permite envio de cookies/autenticação se necessário
-// };
+      
+//     if(whiteList.includes(origin)){
+//         callback(null, true)
+//     }else {
+//         callback(new Error('Erro de Cors'))
+//     }
+//    }
+// }
+
+import { CorsOptions } from "cors";
+
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
+    const whiteList: string[] = [process.env.FRONTEND_URL];
+
+    // Permitir undefined (caso de testes locais ou API interna)
+    if (process.env.NODE_ENV === "development") {
+      whiteList.push(undefined);
+    }
+
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.error(`⚠️ Bloqueio CORS para origem: ${origin}`);
+      callback(new Error("Erro de CORS: Origem não permitida"));
+    }
+  },
+  credentials: true, // Permite envio de cookies/autenticação se necessário
+};
